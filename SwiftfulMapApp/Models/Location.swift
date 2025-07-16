8bcd17e66e28a9ec78e7e5a0e983a4ed2b79df70
shereemorrison <shereemorrison@outlook.com>
2025-07-16 16:35:28 +1000

    Added locations list

--- SwiftfulMapApp/Models/Location.swift
+++ SwiftfulMapApp/Models/Location.swift
@@ -8,11 +8,20 @@
 import Foundation
 import MapKit
 
-struct Location {
+struct Location: Identifiable, Equatable {
+
     let name: String
     let cityName: String
     let coordinates: CLLocationCoordinate2D
     let description: String
     let imageNames: [String]
     let link: String
+    
+    var id: String {
+        name + cityName
+    }
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
---
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  LocationsViewModel.swift
+//  SwiftfulMapApp
+//
+//  Created by Sheree Morrison on 16/7/2025.
+//
+
+import SwiftUI
+import MapKit
+
+class LocationsViewModel: ObservableObject {
+    
+    //All locations
+    @Published var locations: [Location]
+    
+    //Current location on map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+    
+    //Initialise with blank region
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    //Show list of locations
+    @Published var showLocationsList: Bool = false
+    
+    init() {
+        let locations = LocationsDataService.locations
+        self.locations = locations
+        self.mapLocation = locations.first!
+        self.updateMapRegion(location: locations.first!)
+    }
+    
+    //Change from blank region to the current location of whatever the location is
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates, span: mapSpan)
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
+        }
+    }
+}
